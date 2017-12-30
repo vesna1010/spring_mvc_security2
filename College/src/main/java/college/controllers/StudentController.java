b1d0283a74e2af0930395353ffbeada5b3a049b4
Vesna S <vesnasdoboj@gmail.com>
2017-12-30 23:35:57 +0100

    Update StudentController.java

--- College/src/main/java/college/controllers/StudentController.java
+++ College/src/main/java/college/controllers/StudentController.java
@@ -1,100 +1,114 @@
 package college.controllers;
 
-import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import college.enums.Gender;
 import college.model.Student;
 import college.model.StudyProgram;
-import college.service.HibernateService;
+import college.service.StudentService;
+import college.service.StudyProgramService;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
 
+	
 	@Autowired
-	private HibernateService<Student> studentService;
-
+	private StudentService studentService;
 	@Autowired
-	private HibernateService<StudyProgram> studyProgramService;
+	private StudyProgramService studyProgramService;
 
-	//returns all students or by the study program
-	@RequestMapping(method = RequestMethod.GET)
-	public String showStudents(Model model, @RequestParam(required = false) String id) {
-		if (id != null) {
-			StudyProgram studyProgram = studyProgramService.findOne(id);
-			if (studyProgram != null) {
-				model.addAttribute("students", studyProgram.getStudents());
-				model.addAttribute("studyProgram", studyProgram);
-			} else {
-				model.addAttribute("students", null);
-				model.addAttribute("studyProgram", null);
-			}
-		} else {
-			model.addAttribute("students", studentService.findAll());
-			model.addAttribute("studyProgram", null);
-		}
-		return "students";
-	}
-
-	//creates form for new student
-	@RequestMapping("/save")
-	public String showForm(Model model) {
-		model.addAttribute("message", "Add New");
-		model.addAttribute("studyPrograms", studyProgramService.findAll());
-		model.addAttribute("student", new Student());
-		return "studentForm";
-	}
-
-	//save or update student
-	@RequestMapping(value = "/save", method = RequestMethod.POST, consumes = "multipart/form-data")
-	public String saveStudent(@RequestParam("button") String button, @Valid @ModelAttribute("student") Student student, BindingResult result, Model model) {
-		if (!result.hasErrors()) {
-			if (button.equals("save")) {
-				studentService.save(student);
-				return "redirect:/students/save";
-			}
-		}
-		if (button.equals("reset")) {
-			return "redirect:/students/save";
-		}
-		if (studentService.findOne(student.getId()) == null) {
-			model.addAttribute("message", "Add New");
-		} else {
-			model.addAttribute("message", "Update");
-		}
-		model.addAttribute("studyPrograms", studyProgramService.findAll());
-		return "studentForm";
+	@RequestMapping(method = RequestMethod.GET, params = "!studyProgramId")
+	public ModelAndView renderStudentsPageWithAllStudents(ModelAndView model) {
+		model.setViewName("studentsPage");
+		model.addObject("title", "All Students");
+		model.addObject("students", studentService.findAllStudents());
+		
+		return model;
 	}
 	
-	//returns student with specified ID
-	@RequestMapping("/find/{id}")
-	public String showStudent(Model model, @PathVariable String id) throws UnsupportedEncodingException {
-		model.addAttribute("message", "Update");
-		model.addAttribute("studyPrograms", studyProgramService.findAll());
-		model.addAttribute("student", studentService.findOne(id));
-		return "studentForm";
+	@RequestMapping(method = RequestMethod.GET, params = "studyProgramId")
+	public ModelAndView renderStudentsPageWithStudentsByStudyProgram(@RequestParam String studyProgramId) {
+		return getModelAndViewByStudyProgram(studyProgramService.findStudyProgramById(studyProgramId));
 	}
 
-	//returns all exams by student
-	@RequestMapping("/delete/{id}")
-	public String deleteStudent(Model model, @PathVariable String id) {
-		studentService.delete(id);
-		return "redirect:/students/";
+	private ModelAndView getModelAndViewByStudyProgram(StudyProgram studyProgram) {
+		ModelAndView model = new ModelAndView("studentsPage");
+
+		model.addObject("title", "Students at " + studyProgram.getTitle());
+		model.addObject("students", studyProgram.getStudents());
+
+		return model;
+
+	}
+	@RequestMapping("/studentForm")
+	public ModelAndView renderEmptyStudentForm(ModelAndView model) {
+		model.setViewName("studentForm");
+		model.addObject("student", new Student());
+		model.addObject("genders", Arrays.asList(Gender.values()));
+		model.addObject("studyPrograms", studyProgramService.findAllStudyPrograms());
+		
+		return model;
 	}
 
-	//returns exams with specified student
-	@RequestMapping("/exams/{id}")
-	public String studentExams(Model model, @PathVariable String id) {
-		model.addAttribute("student", studentService.findOne(id));
-		return "studentExams";
+	@RequestMapping(value = "/save", method = RequestMethod.POST, consumes = "multipart/form-data")
+	public ModelAndView saveStudentAndRenderStudentForm(@Valid @ModelAttribute Student student, 
+			BindingResult result) {
+		
+		if (!result.hasErrors()) {
+			return saveStudentAndGetModelAndView(student);
+		}
+	
+		return new ModelAndView("studentForm", getModelMap());
+	}
+	
+	private ModelAndView saveStudentAndGetModelAndView(Student student) {
+		studentService.saveOrUpdateStudent(student);
+		
+		return new ModelAndView("redirect:/students/studentForm");
+	}
+	
+	private Map<String, Object> getModelMap() {
+		Map<String, Object> modelMap = new HashMap<>();
+
+		modelMap.put("genders", Arrays.asList(Gender.values()));
+		modelMap.put("studyPrograms", studyProgramService.findAllStudyPrograms());
+
+		return modelMap;
+	}
+	
+	@RequestMapping("/edit/{studentId}")
+	public ModelAndView renderStudentFormWithStudent(@PathVariable String studentId) {
+		ModelAndView model = new ModelAndView("studentForm");
+		
+		model.addObject("student", studentService.findStudentById(studentId));
+		model.addObject("genders", Arrays.asList(Gender.values()));
+		model.addObject("studyPrograms", studyProgramService.findAllStudyPrograms());
+		
+		return model;
+	}
+
+	@RequestMapping("/delete/{studentId}")
+	public ModelAndView deleteStudentAndRenderStudentsPage(@PathVariable String studentId) {
+		studentService.deleteStudentById(studentId);
+		
+		return new ModelAndView("redirect:/students");
+	}
+
+	@RequestMapping("/exams/{studentId}")
+	public ModelAndView renderStudentExamsPageWithStudent(@PathVariable String studentId) {	
+		return new ModelAndView("studentExamsPage", "student", studentService.findStudentById(studentId));
 	}
 
 }
