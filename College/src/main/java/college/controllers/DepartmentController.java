f8c9fb0ffa56429b9d4682439c6377dd38da3e87
Vesna S <vesnasdoboj@gmail.com>
2017-12-30 23:29:24 +0100

    Update DepartmentController.java

--- College/src/main/java/college/controllers/DepartmentController.java
+++ College/src/main/java/college/controllers/DepartmentController.java
@@ -3,73 +3,59 @@ package college.controllers;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import college.model.Department;
-import college.service.HibernateService;
+import college.service.DepartmentService;
 
 @Controller
 @RequestMapping("/departments")
 public class DepartmentController {
 
 	@Autowired
-	private HibernateService<Department> departmentService;
+	private DepartmentService departmentService;
 
-	
-	//returns departments
-	@RequestMapping(method = RequestMethod.GET)
-	public String showAllDepartements(Model model) {
-		model.addAttribute("departments", departmentService.findAll());
-		return "departments";
+	@RequestMapping
+	public ModelAndView renderDepartmentsPageWithAllDepartments() {
+		return new ModelAndView("departmentsPage", "departments", departmentService.findAllDepartments());
 	}
 
-	//creates form for new department
-	@RequestMapping("/save")
-	public String showDepartmentForm(Model model) {
-		model.addAttribute("message", "Add New");
-		model.addAttribute("department", new Department());
-		return "departmentForm";
+	@RequestMapping(value = "/departmentForm", method = RequestMethod.GET)
+	public ModelAndView renderEmptyDepartmentForm() {
+		return new ModelAndView("departmentForm", "department", new Department());
 	}
 
-	//save or update department
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String saveDepartment(@RequestParam("button") String button, Model model,
-			@Valid @ModelAttribute("department") Department department, BindingResult result) {
+	public ModelAndView saveDepartmentAndRenderDepartmentForm(@Valid @ModelAttribute Department department, 
+		BindingResult result) {
+		
 		if (!result.hasErrors()) {
-			if (button.equals("save")) {
-				departmentService.save(department);
-				return "redirect:/departments/save";
-			}
+			return saveDepartmentAndGetModelAndView(department);
 		}
-		if(button.equals("reset")){
-			return "redirect:/departments/save";
-		}
-		if(departmentService.findOne(department.getId())==null){
-			model.addAttribute("message", "Add New");
-		}else{
-			model.addAttribute("message", "Update");
-		}
-		return "departmentForm";
+
+		return new ModelAndView("departmentForm");
 	}
 
-	//returns department with specified ID
-	@RequestMapping("/find/{id}")
-	public String showDep(Model model, @PathVariable String id) {
-		model.addAttribute("message", "Update");
-		model.addAttribute("department", departmentService.findOne(id));
-		return "departmentForm";
+	private ModelAndView saveDepartmentAndGetModelAndView(Department department) {
+		departmentService.saveOrUpdateDepartment(department);
+
+		return new ModelAndView("redirect:/departments/departmentForm");
 	}
 
-	//deletes department with specified ID
-	@RequestMapping("/delete/{id}")
-	public String deleteDep(Model model, @PathVariable String id) {
-		departmentService.delete(id);
-		return "redirect:/departments/";
+	@RequestMapping("/edit/{departmentId}")
+	public ModelAndView renderDepartmentFormWithDepartment(@PathVariable String departmentId) {
+		return new ModelAndView("departmentForm", "department", departmentService.findDepartmentById(departmentId));
+	}
+
+	@RequestMapping("/delete/{departmentId}")
+	public ModelAndView deleteDepartmentAndRenderDepartmentsPage(@PathVariable String departmentId) {
+		departmentService.deleteDepartmentById(departmentId);
+
+		return new ModelAndView("redirect:/departments");
 	}
 
 }
