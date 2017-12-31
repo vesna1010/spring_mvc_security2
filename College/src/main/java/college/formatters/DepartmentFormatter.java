8cf21c3ad7050fd483e8e7be103b706405885ebc
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:02:28 +0100

    Update DepartmentFormatter.java

--- College/src/main/java/college/formatters/DepartmentFormatter.java
+++ College/src/main/java/college/formatters/DepartmentFormatter.java
@@ -4,28 +4,26 @@ import java.text.ParseException;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
-import college.model.Department;
-import college.service.HibernateService;
+import org.springframework.stereotype.Component;
 
+import college.model.Department;
+import college.service.DepartmentService;
+
+@Component
 public class DepartmentFormatter implements Formatter<Department> {
 
 	@Autowired
-	private HibernateService<Department> departmentService;
+	private DepartmentService departmentService;
 
 	@Override
 	public String print(Department department, Locale locale) {
-		if (department == null){
-			return null;
-		}
 		return department.getTitle();
 	}
-		
+
 	@Override
-	public Department parse(String id, Locale locale) throws ParseException {
-		if(id==null){
-			return null;
-		}
-		return departmentService.findOne(id);
+	public Department parse(String departmentId, Locale locale) throws ParseException {
+		return departmentService.findDepartmentById(departmentId);
 	}
 
 }
+
