7b977866257a011161c43d064604f2f9789a8a61
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:01:33 +0100

    Update AddressFormatter.java

--- College/src/main/java/college/formatters/AddressFormatter.java
+++ College/src/main/java/college/formatters/AddressFormatter.java
@@ -3,32 +3,28 @@ package college.formatters;
 import java.text.ParseException;
 import java.util.Locale;
 import org.springframework.format.Formatter;
-
+import org.springframework.stereotype.Component;
 import college.model.Address;
 
+@Component
 public class AddressFormatter implements Formatter<Address> {
 
 	@Override
 	public String print(Address address, Locale locale) {
-		if (address == null) {
-			return null;
-		}
 		return address.toString();
 	}
 
 	@Override
 	public Address parse(String text, Locale locale) throws ParseException {
-		Address address = new Address();
-		String[] array = { "", "", "" };
-		if (text != null) {
-			String[] parts = text.split("-");
-			for (int i = 0; i < parts.length; i++)
-				array[i] = parts[i].trim();
+		String[] arrayAddress = { "", "", "" };
+		String[] parts = text.split("-");
+
+		for (int i = 0; i < parts.length; i++) {
+			arrayAddress[i] = parts[i].trim();
 		}
-		address.setCity(array[0]);
-		address.setStreet(array[1]);
-		address.setState(array[2]);
-		return address;
+
+		return new Address(arrayAddress[0], arrayAddress[1], arrayAddress[2]);
 	}
 
 }
+
