c86c1bf653b8fc62d0193dbb8cf80baa0bc42f21
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:11:00 +0100

    Update UserServiceImpl.java

--- College/src/main/java/college/service/impl/UserServiceImpl.java
+++ College/src/main/java/college/service/impl/UserServiceImpl.java
@@ -1,62 +1,62 @@
 package college.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import javax.annotation.Resource;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import college.dao.UserDao;
-import college.model.Role;
+import college.dao.extensions.UserDao;
+import college.enums.Role;
 import college.model.User;
 import college.service.UserService;
 
-@Service("userService")
-public class UserServiceImpl implements UserService {
-	
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
 	@Resource
 	private UserDao userDao;
-	
+
 	@Override
-	public List<User> findUsers() {
-		return userDao.findUsers();
+	public Set<User> findUsers() {
+		return userDao.findAll();
 	}
 
 	@Override
-	public User findUser(String username) {
-		return userDao.findUser(username);
+	public User findUserByUsername(String username) {
+		return userDao.findOneById(username);
 	}
 
 	@Override
-	public void saveOrUpdate(User user) {
+	public void saveOrUpdateUser(User user) {
 		userDao.saveOrUpdate(user);
 	}
 
 	@Override
-	public void deleteUser(String username) {
-		userDao.deleteUser(username);
+	public void deleteUserByUsername(String username) {
+		userDao.deleteById(username);
 	}
 
 	@Override
-	public void disableUser(String username) {
-		userDao.disableUser(username);
+	public void disableUserByUsername(String username) {
+		userDao.disableByUsername(username);
 	}
 
 	@Override
-	public List<Role> findRoles() {
-		return userDao.findRoles();
-	}
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userDao.findOneById(username);
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 
-	@Override
-	public Role findRole(String id) {
-		return userDao.findRole(id);
-	}
+		for (Role role : user.getRoles()) {
+			authorities.add(new SimpleGrantedAuthority("ROLE_" + role.toString()));
+		}
 
-	@Override
-	public void saveOrUpdate(Role role) {
-		userDao.saveOrUpdate(role);
+		return new org.springframework.security.core.userdetails.User(
+				user.getUsername(), user.getPassword(), authorities);
 	}
-
-	@Override
-	public void deleteFromTables() {
-		userDao.deleteFromTables();
-	}
-
 }
