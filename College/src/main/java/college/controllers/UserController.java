6833438066d31184d032354bad4c7406b963570e
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 03:48:44 +0100

    Update UserController.java

--- College/src/main/java/college/controllers/UserController.java
+++ College/src/main/java/college/controllers/UserController.java
@@ -3,7 +3,6 @@ package college.controllers;
 import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
