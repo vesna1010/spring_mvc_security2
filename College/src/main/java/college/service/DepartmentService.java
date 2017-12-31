7063c54d35374ed7c563aa6837a32dda737a951e
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:06:21 +0100

    Create DepartmentService.java

---
+++ College/src/main/java/college/service/DepartmentService.java
@@ -0,0 +1,16 @@
+package college.service;
+
+import java.util.Set;
+import college.model.Department;
+
+public interface DepartmentService {
+
+	Set<Department> findAllDepartments();
+
+	Department findDepartmentById(String id);
+	
+	void saveOrUpdateDepartment(Department department);
+
+	void deleteDepartmentById(String id);
+
+}
