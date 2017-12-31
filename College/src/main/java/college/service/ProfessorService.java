3894557e43e0660cfd180b9f4aec639c05f59cd3
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:07:17 +0100

    Create ProfessorService.java

---
+++ College/src/main/java/college/service/ProfessorService.java
@@ -0,0 +1,16 @@
+package college.service;
+
+import java.util.Set;
+import college.model.Professor;
+
+public interface ProfessorService {
+
+	Set<Professor> findAllProfessors();
+	
+	Professor findProfessorById(String id);
+	
+	void deleteProfessorById(String id);
+	
+	void saveOrUpdateProfessor(Professor professor);
+	
+}
