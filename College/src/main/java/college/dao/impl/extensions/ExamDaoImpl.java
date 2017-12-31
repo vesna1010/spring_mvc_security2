7106ee84515de8d05d1f24a94bc0055d90f2540f
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 03:56:17 +0100

    Create ExamDaoImpl.java

---
+++ College/src/main/java/college/dao/impl/extensions/ExamDaoImpl.java
@@ -0,0 +1,34 @@
+package college.dao.impl.extensions;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import org.hibernate.Query;
+import org.springframework.stereotype.Component;
+import college.dao.extensions.ExamDao;
+import college.dao.impl.HibernateDaoImpl;
+import college.model.Exam;
+import college.model.Professor;
+import college.model.Subject;
+
+@Component("examDao")
+public class ExamDaoImpl extends HibernateDaoImpl<Long, Exam> implements ExamDao {
+
+	public ExamDaoImpl() {
+		setEntityClass(Exam.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Set<Exam> findExamsByObjects(Professor professor, Subject subject, Date date) {
+		Query query = getSession().createQuery(
+				"from Exam where professor=:professor and date=:date and subject=:subject");
+		
+		query.setEntity("professor", professor);
+		query.setEntity("subject", subject);
+		query.setDate("date", date);
+		
+		return new HashSet<Exam>(query.list());
+	}
+
+}
