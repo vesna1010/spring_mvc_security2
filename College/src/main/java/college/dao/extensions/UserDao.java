7f61628c017f9c3976dc601132ed2ce537e41924
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 03:54:13 +0100

    Create UserDao.java

---
+++ College/src/main/java/college/dao/extensions/UserDao.java
@@ -0,0 +1,9 @@
+package college.dao.extensions;
+
+import college.dao.HibernateDao;
+import college.model.User;
+
+public interface UserDao extends HibernateDao<String, User> {
+
+	void disableByUsername(String username);
+}
