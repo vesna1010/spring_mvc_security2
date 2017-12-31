50b9267897a785ea29e588f9d1fa1f27717a63e9
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 21:34:55 +0100

    Update TitleImpl.java

--- College/src/main/java/college/validation/TitleImpl.java
+++ College/src/main/java/college/validation/TitleImpl.java
@@ -1,24 +1,20 @@
 package college.validation;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-public class TitleImpl implements ConstraintValidator<Title, String>{
+
+public class TitleImpl implements ConstraintValidator<Title, String> {
 
 	private String pattern;
 
 	@Override
 	public void initialize(Title constraintAnnotation) {
-		pattern=constraintAnnotation.pattern();
-		
+		pattern = constraintAnnotation.pattern();
 	}
 
 	@Override
 	public boolean isValid(String title, ConstraintValidatorContext context) {
-		if(title.matches(pattern)){
-			return true;
-		}else{
-			return false;
-		}
+		return title.matches(pattern);
 	}
-	
-	
+
 }
