e43d8f9b354e2cf23d889fe09c8b7753817398d0
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:13:54 +0100

    Create ExamServiceImpl.java

---
+++ College/src/main/java/college/service/impl/ExamServiceImpl.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import college.dao.extensions.ExamDao;
+import college.model.Exam;
+import college.model.Professor;
+import college.model.Subject;
+import college.service.ExamService;
+
+@Service
+public class ExamServiceImpl implements ExamService {
+
+	@Resource
+	private ExamDao examDao;
+
+	@Override
+	public void saveOrUpdateExam(Exam exam) {
+		examDao.saveOrUpdate(exam);
+	}
+
+	@Override
+	public Exam findExamById(Long id) {
+		return examDao.findOneById(id);
+	}
+
+	@Override
+	public void deleteExamById(Long id) {
+		examDao.deleteById(id);
+	}
+
+	@Override
+	public Set<Exam> findExamsByObjects(Professor professor, Subject subject, Date date) {
+		return examDao.findAll();
+	}
+
+}
