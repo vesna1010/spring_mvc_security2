63a3f9e3e306be3c219f9915bfc2d3cd5bffd9a4
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 21:34:08 +0100

    Update PersonNameImpl.java

--- College/src/main/java/college/validation/PersonNameImpl.java
+++ College/src/main/java/college/validation/PersonNameImpl.java
@@ -1,23 +1,20 @@
 package college.validation;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-public class PersonNameImpl implements ConstraintValidator<PersonName, String>{
+
+public class PersonNameImpl implements ConstraintValidator<PersonName, String> {
 
 	private String pattern;
-	
+
 	@Override
 	public void initialize(PersonName constraintAnnotation) {
-		pattern=constraintAnnotation.pattern();
+		pattern = constraintAnnotation.pattern();
 	}
 
 	@Override
 	public boolean isValid(String name, ConstraintValidatorContext context) {
-			if(name.matches(pattern)){
-				return true;
-			}else{
-				return false;
-			}
-		
+		return name.matches(pattern);
 	}
 
 }
