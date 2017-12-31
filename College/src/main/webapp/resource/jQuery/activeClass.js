aaff851917c3a7f0555010d0a6218df7e84f4d94
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:42:37 +0100

    Update activeClass.js

--- College/src/main/webapp/resource/jQuery/activeClass.js
+++ College/src/main/webapp/resource/jQuery/activeClass.js
@@ -1,8 +1,11 @@
-//add active class
-$(document).ready(function(){
-	var active=$("[name='page']").val();
-	$("ul.navbar-nav li").each(function(){
-		if($(this).attr("id")==active)
+$(document).ready(function() {
+	
+	var active = $("[name='page']").val();
+	
+	$("ul.navbar-nav li").each(function() {
+		if ($(this).attr("id") == active) {
 			$(this).addClass("active");
+		}
 	});
+	
 });
