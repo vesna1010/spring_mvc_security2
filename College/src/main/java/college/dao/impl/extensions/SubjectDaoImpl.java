7875a7eafeedfab52d999c1d1b2673c64ed397a7
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 03:59:05 +0100

    Create SubjectDaoImpl.java

---
+++ College/src/main/java/college/dao/impl/extensions/SubjectDaoImpl.java
@@ -0,0 +1,15 @@
+package college.dao.impl.extensions;
+
+import org.springframework.stereotype.Component;
+import college.dao.extensions.SubjectDao;
+import college.dao.impl.HibernateDaoImpl;
+import college.model.Subject;
+
+@Component("subjectDao")
+public class SubjectDaoImpl extends HibernateDaoImpl<String, Subject> implements SubjectDao {
+
+	public SubjectDaoImpl() {
+		setEntityClass(Subject.class);
+	}
+
+}
