71489dcc42edd8b37b8c5f8abb850dab04374d67
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 03:39:56 +0100

    Create DepartmentDao.java

---
+++ College/src/main/java/college/dao/extensions/DepartmentDao.java
@@ -0,0 +1,8 @@
+package college.dao.extensions;
+
+import college.dao.HibernateDao;
+import college.model.Department;
+
+public interface DepartmentDao extends HibernateDao<String, Department> {
+
+}
