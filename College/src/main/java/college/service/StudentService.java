9553230f8b87da6ce6865256927c0dd57f68c441
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:07:43 +0100

    Create StudentService.java

---
+++ College/src/main/java/college/service/StudentService.java
@@ -0,0 +1,16 @@
+package college.service;
+
+import java.util.Set;
+import college.model.Student;
+
+public interface StudentService {
+
+	Set<Student> findAllStudents();
+	
+	Student findStudentById(String id);
+	
+	void deleteStudentById(String id);
+	
+	void saveOrUpdateStudent(Student student);
+	
+}
