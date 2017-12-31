b9adea6436f2df77b90f6ecd167bb5043dab15c8
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:12:51 +0100

    Create StudentServiceImpl.java

---
+++ College/src/main/java/college/service/impl/StudentServiceImpl.java
@@ -0,0 +1,36 @@
+package college.service.impl;
+
+import java.util.Set;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import college.dao.extensions.StudentDao;
+import college.model.Student;
+import college.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+	@Resource
+	private StudentDao studentDao;
+
+	@Override
+	public Set<Student> findAllStudents() {
+		return studentDao.findAll();
+	}
+
+	@Override
+	public Student findStudentById(String id) {
+		return studentDao.findOneById(id);
+	}
+
+	@Override
+	public void deleteStudentById(String id) {
+		studentDao.deleteById(id);
+	}
+
+	@Override
+	public void saveOrUpdateStudent(Student student) {
+		studentDao.saveOrUpdate(student);
+	}
+
+}
