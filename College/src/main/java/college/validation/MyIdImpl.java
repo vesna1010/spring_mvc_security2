8c934e4754111c1f7d46068800b4b38bbabc23dc
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 21:33:09 +0100

    Update MyIdImpl.java

--- College/src/main/java/college/validation/MyIdImpl.java
+++ College/src/main/java/college/validation/MyIdImpl.java
@@ -3,22 +3,18 @@ package college.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class MyIdImpl implements ConstraintValidator<MyId, String>{
+public class MyIdImpl implements ConstraintValidator<MyId, String> {
 
 	private String pattern;
-	
+
 	@Override
 	public void initialize(MyId constraintAnnotation) {
-		pattern=constraintAnnotation.pattern();
-		
+		pattern = constraintAnnotation.pattern();
 	}
 
 	@Override
 	public boolean isValid(String id, ConstraintValidatorContext context) {
-		if(id.matches(pattern))
-			return true;
-		else
-			return false;
+		return id.matches(pattern);
 	}
 
 }
