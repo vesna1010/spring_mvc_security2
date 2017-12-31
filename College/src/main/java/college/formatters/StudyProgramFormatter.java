c6ae1ea13f8d24c5c3d342644ef13a3a9f870b18
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 04:03:59 +0100

    Update StudyProgramFormatter.java

--- College/src/main/java/college/formatters/StudyProgramFormatter.java
+++ College/src/main/java/college/formatters/StudyProgramFormatter.java
@@ -4,28 +4,24 @@ import java.text.ParseException;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
 import college.model.StudyProgram;
-import college.service.HibernateService;
+import college.service.StudyProgramService;
 
+@Component
 public class StudyProgramFormatter implements Formatter<StudyProgram> {
 
 	@Autowired
-	private HibernateService<StudyProgram> studyProgramService;
+	private StudyProgramService studyProgramService;
 
 	@Override
 	public String print(StudyProgram studyProgram, Locale locale) {
-		if (studyProgram == null){
-			return null;
-		};
-			return studyProgram.getTitle();
+		return studyProgram.getTitle();
 	}
 
 	@Override
-	public StudyProgram parse(String id, Locale locale) throws ParseException {
-		if (id == null){
-			return null;
-		}
-		return studyProgramService.findOne(id);
+	public StudyProgram parse(String studyProgramId, Locale locale) throws ParseException {
+		return studyProgramService.findStudyProgramById(studyProgramId);
 	}
 
 }
