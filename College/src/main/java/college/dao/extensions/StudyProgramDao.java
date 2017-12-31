ec155cf404747041b9a4d8c3b6b2c2abc83ce21f
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 03:53:14 +0100

    Create StudyProgramDao.java

---
+++ College/src/main/java/college/dao/extensions/StudyProgramDao.java
@@ -0,0 +1,8 @@
+package college.dao.extensions;
+
+import college.dao.HibernateDao;
+import college.model.StudyProgram;
+
+public interface StudyProgramDao extends HibernateDao<String, StudyProgram> {
+
+}
