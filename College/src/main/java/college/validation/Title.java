11cf90f7019ed25e97e3951b9579fb087f78a965
Vesna S <vesnasdoboj@gmail.com>
2017-12-31 21:34:35 +0100

    Update Title.java

--- College/src/main/java/college/validation/Title.java
+++ College/src/main/java/college/validation/Title.java
@@ -1,4 +1,5 @@
 package college.validation;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -9,7 +10,7 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ METHOD, FIELD, ANNOTATION_TYPE})
+@Target({ METHOD, FIELD, ANNOTATION_TYPE })
 @Retention(RUNTIME)
 @Documented
 @Constraint(validatedBy = college.validation.TitleImpl.class)
@@ -17,10 +18,10 @@ public @interface Title {
 
 	String message() default "";
 
-	Class<?>[] groups() default { };
+	Class<?>[] groups() default {};
 
-	Class<? extends Payload>[] payload() default { };
-	
-	String pattern() default "^([a-zA-Z0-9]+(\\s+)?){5,}$";
+	Class<? extends Payload>[] payload() default {};
+
+	String pattern() default "^([a-zA-Z0-9]{3,}(\\s)*)+$";
 
 }
