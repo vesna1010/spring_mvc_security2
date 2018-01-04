7152823ca2e04cb8fd8574b0262dceaed4ba82c8
Vesna S <vesnasdoboj@gmail.com>
2018-01-04 03:08:07 +0100

    Update Address.java

--- College/src/main/java/college/model/Address.java
+++ College/src/main/java/college/model/Address.java
@@ -7,17 +7,9 @@ import javax.validation.constraints.Pattern;
 @Embeddable
 public class Address {
 
-	@Pattern(regexp="^([a-zA-Z]+\\s?){3,}$")
-	@Column(name = "STATE")
-	private String state;
-
-	@Pattern(regexp="^([a-zA-Z]+\\s?){3,}$")
-	@Column(name = "CITY")
-	private String city;
-
-	@Pattern(regexp="^([a-zA-Z0-9]+\\s?){3,}$")
-	@Column(name = "STREET")
-	private String street;
+	private String city = "";
+	private String street = "";
+	private String state = "";
 
 	public Address() {
 	}
@@ -28,6 +20,8 @@ public class Address {
 		this.state = state;
 	}
 
+	@Pattern(regexp = "^([a-zA-Z]+\\s?){3,}$")
+	@Column(name = "STATE")
 	public String getState() {
 		return state;
 	}
@@ -36,6 +30,8 @@ public class Address {
 		this.state = state;
 	}
 
+	@Pattern(regexp = "^([a-zA-Z]+\\s?){3,}$")
+	@Column(name = "CITY")
 	public String getCity() {
 		return city;
 	}
@@ -44,6 +40,8 @@ public class Address {
 		this.city = city;
 	}
 
+	@Pattern(regexp = "^([a-zA-Z0-9]+\\s?){3,}$")
+	@Column(name = "STREET")
 	public String getStreet() {
 		return street;
 	}
@@ -54,44 +52,7 @@ public class Address {
 
 	@Override
 	public String toString() {
-		return city + "  -  " + street + "  -  " + state;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + ((state == null) ? 0 : state.hashCode());
-		result = prime * result + ((street == null) ? 0 : street.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Address other = (Address) obj;
-		if (city == null) {
-			if (other.city != null)
-				return false;
-		} else if (!city.equals(other.city))
-			return false;
-		if (state == null) {
-			if (other.state != null)
-				return false;
-		} else if (!state.equals(other.state))
-			return false;
-		if (street == null) {
-			if (other.street != null)
-				return false;
-		} else if (!street.equals(other.street))
-			return false;
-		return true;
+		return city + " - " + street + " - " + state;
 	}
 
 }
