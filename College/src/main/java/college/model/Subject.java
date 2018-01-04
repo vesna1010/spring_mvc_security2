5b881bdebdc0ee3e2c979c091f74ddd657b64cfc
Vesna S <vesnasdoboj@gmail.com>
2018-01-04 03:18:02 +0100

    Update Subject.java

--- College/src/main/java/college/model/Subject.java
+++ College/src/main/java/college/model/Subject.java
@@ -1,61 +1,46 @@
 package college.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
-
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 import college.validation.MyId;
 import college.validation.Title;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "SUBJECTS")
 public class Subject implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-
-	@Id
-	@MyId
-	@Column(name = "ID")
-	private String id;
-
-	@Title
-	@Column(name = "TITLE")
-	private String title;
-
-	@NotNull
-	@ManyToOne
-	@JoinColumn(name = "ST_PROGRAM_ID")
+	private String id = "";
+	private String title = "";
 	private StudyProgram studyProgram;
-
-	@OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<SubjectProfessor> subjectsProfessors;
-
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "subject")
-	private List<Exam> exams;
+	private Set<Lecture> lectures = new HashSet<>();
+	private Set<Exam> exams = new HashSet<>();
 
 	public Subject() {
-		exams = new ArrayList<Exam>();
-		subjectsProfessors = new ArrayList<SubjectProfessor>();
 	}
-
+	
 	public Subject(String id, String title, StudyProgram studyProgram) {
-		exams = new ArrayList<Exam>();
-		subjectsProfessors = new ArrayList<SubjectProfessor>();
-		this.id=id;
+		this.id = id;
 		this.title = title;
 		this.studyProgram = studyProgram;
 	}
 
-	
+	@Id
+	@MyId
+	@Column(name = "ID")
 	public String getId() {
 		return id;
 	}
@@ -64,7 +49,8 @@ public class Subject implements Serializable {
 		this.id = id;
 	}
 
-	
+	@Title
+	@Column(name = "TITLE")
 	public String getTitle() {
 		return title;
 	}
@@ -72,7 +58,11 @@ public class Subject implements Serializable {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	
+	@NotNull
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "STUDY_PROGRAM_ID")
+	@Cascade(CascadeType.SAVE_UPDATE)
 	public StudyProgram getStudyProgram() {
 		return studyProgram;
 	}
@@ -81,28 +71,47 @@ public class Subject implements Serializable {
 		this.studyProgram = studyProgram;
 	}
 
-	
-
-	public List<SubjectProfessor> getSubjectsProfessors() {
-		return subjectsProfessors;
-	}
-
-	public List<Exam> getExams() {
+	@OneToMany(mappedBy = "subject", fetch = FetchType.LAZY)
+	@Cascade(CascadeType.ALL)
+	public Set<Exam> getExams() {
 		return exams;
 	}
-
-	public void setExams(List<Exam> exams) {
+	
+	void setExams(Set<Exam> exams) {
 		this.exams = exams;
 	}
+	
+	public void addExam(Exam exam) {
+		exam.setSubject(this);
+		exams.add(exam);
+	}
 
-	//returns list professors for specified subject
-	public List<Professor> getProfessors() {
-		List<Professor> professors = new ArrayList<Professor>();
-		for (SubjectProfessor subProf : this.subjectsProfessors)
-			if(!professors.contains(subProf.getProfessor()))
-			professors.add(subProf.getProfessor());
+	@OneToMany(mappedBy = "subject", fetch = FetchType.LAZY)
+	@Cascade(CascadeType.ALL)
+	public Set<Lecture> getLectures() {
+		return lectures;
+	}
+
+	void setLectures(Set<Lecture> lectures) {
+		this.lectures = lectures;
+	}
+	
+	public void addLecture(Lecture lecture) {
+		lecture.setSubject(this);
+		this.lectures.add(lecture);
+	}
+
+	@Transient
+	public Set<Professor> getProfessors() {
+		Set<Professor> professors = new HashSet<>();
+
+		for (Lecture subjectProfessor : this.lectures) {
+			professors.add(subjectProfessor.getProfessor());
+		}
+
 		return professors;
 	}
+	
 
 	@Override
 	public int hashCode() {
@@ -141,7 +150,4 @@ public class Subject implements Serializable {
 		return true;
 	}
 
-	
-
-	
 }
