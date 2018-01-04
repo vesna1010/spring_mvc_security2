1da652798e238ed6b91aa945f52dc2a88ffa7ea8
Vesna S <vesnasdoboj@gmail.com>
2018-01-04 03:23:36 +0100

    Create LectureDaoImpl.java

---
+++ College/src/main/java/college/dao/impl/extensions/LectureDaoImpl.java
@@ -0,0 +1,15 @@
+package college.dao.impl.extensions;
+
+import org.springframework.stereotype.Component;
+import college.dao.extensions.LectureDao;
+import college.dao.impl.HibernateDaoImpl;
+import college.model.Lecture;
+
+@Component("lectureDao")
+public class LectureDaoImpl extends HibernateDaoImpl<Long, Lecture> implements LectureDao {
+
+	public LectureDaoImpl() {
+		setEntityClass(Lecture.class);
+	}
+
+}
