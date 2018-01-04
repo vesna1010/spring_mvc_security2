e08cefe19b40492f6a5c5ba5bf33bf0c77a59095
Vesna S <vesnasdoboj@gmail.com>
2018-01-04 03:17:26 +0100

    Update StudyProgram.java

--- College/src/main/java/college/model/StudyProgram.java
+++ College/src/main/java/college/model/StudyProgram.java
@@ -1,86 +1,55 @@
 package college.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import javax.persistence.CascadeType;
+import java.util.Set;
+import java.util.HashSet;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import org.hibernate.annotations.Formula;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 import org.springframework.format.annotation.DateTimeFormat;
 import college.validation.MyId;
 import college.validation.Title;
 
+@SuppressWarnings({ "serial" })
 @Entity
-@Table(name="STUDY_PROGRAMS")
+@Table(name = "STUDY_PROGRAMS")
 public class StudyProgram implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	
+	private String id = "";
+	private String title = "";
+	private Date dateOfCreation = new Date();
+	private Integer durationOfStudy = 3;
+	private Department department;
+	private Set<Student> students = new HashSet<>();
+	private Set<Subject> subjects = new HashSet<>();
+
+	public StudyProgram() {
+	}
+
+	public StudyProgram(String id, String title, Department department) {
+		this.id = id;
+		this.title = title;
+		this.department = department;
+	}
 
 	@Id
 	@MyId
-	@Column(name="ID")
-	private String id;
-
-	@Title
-	@Column(name="TITLE")
-	private String title;
-
-	@NotNull
-	@DateTimeFormat(pattern = "dd-MM-yyyy")
-	@Column(name="DATE_OF_CREATION")
-	@Temporal(TemporalType.DATE)
-	private Date dateOfCreation;
-	
-	@NotNull
-	@Min(value = 1)
-	@Max(value = 5)
-	@Column(name="DURATION_OF_STUDY")
-	private Integer durationOfStudy;
-	
-	@Formula("DURATION_OF_STUDY*60")
-	@Column(name="ECTS")
-	private Integer ects;
-	
-	@NotNull
-	@ManyToOne
-	@JoinColumn(name="DEPARTMENT_ID")
-	private Department department;
-
-	@OrderBy("yearOfStudy ASC")
-	@OneToMany(mappedBy="studyProgram", cascade=CascadeType.ALL)
-	private List<Student> students;
-	
-	@OneToMany(mappedBy="studyProgram", cascade=CascadeType.ALL)
-	private List<Subject> subjects;
-
-	public StudyProgram(){
-		students=new ArrayList<Student>();
-		subjects=new ArrayList<Subject>();
-	}
-
-	public StudyProgram(String id, String title, Date dateOfCreation, Department department, Integer durationOfStudy) {
-		students=new ArrayList<Student>();
-		subjects=new ArrayList<Subject>();
-		this.id = id;
-		this.title = title;
-		this.dateOfCreation = dateOfCreation;
-		this.department = department;
-		this.durationOfStudy=durationOfStudy;
-	}
-
+	@Column(name = "ID")
 	public String getId() {
 		return id;
 	}
@@ -89,6 +58,8 @@ public class StudyProgram implements Serializable {
 		this.id = id;
 	}
 
+	@Title
+	@Column(name = "TITLE")
 	public String getTitle() {
 		return title;
 	}
@@ -97,6 +68,9 @@ public class StudyProgram implements Serializable {
 		this.title = title;
 	}
 
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	@Temporal(TemporalType.DATE)
+	@Column(name = "DATE_OF_CREATION")
 	public Date getDateOfCreation() {
 		return dateOfCreation;
 	}
@@ -105,6 +79,10 @@ public class StudyProgram implements Serializable {
 		this.dateOfCreation = dateOfCreation;
 	}
 
+	@NotNull
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "DEPARTMENT_ID")
+	@Cascade(CascadeType.SAVE_UPDATE)
 	public Department getDepartment() {
 		return department;
 	}
@@ -113,26 +91,40 @@ public class StudyProgram implements Serializable {
 		this.department = department;
 	}
 
-	public List<Student> getStudents() {
-		List<Student> activeStudents=new ArrayList<Student>();
-		for(Student student:this.students)
-			if(student.getDateOfCompletion()==null)
-				activeStudents.add(student);
-		return activeStudents;
+	@OneToMany(mappedBy = "studyProgram", fetch = FetchType.LAZY)
+	@Cascade(CascadeType.ALL)
+	public Set<Student> getStudents() {
+		return students;
 	}
-
-	public void setStudents(List<Student> students) {
+	
+	void setStudents(Set<Student> students) {
 		this.students = students;
 	}
-
-	public List<Subject> getSubjects() {
-		return subjects;
+	
+	public void addStudent(Student student) {
+		student.setStudyProgram(this);
+		this.students.add(student);
 	}
 
-	public void setSubjects(List<Subject> subjects) {
+	@OneToMany(mappedBy = "studyProgram", fetch = FetchType.LAZY)
+	@Cascade(CascadeType.ALL)
+	public Set<Subject> getSubjects() {
+		return subjects;
+	}
+	
+	void setSubjects(Set<Subject> subjects) {
 		this.subjects = subjects;
 	}
 	
+	public void addSubject(Subject subject) {
+		subject.setStudyProgram(this);
+		this.subjects.add(subject);
+	}
+
+	@NotNull
+	@Min(value = 1)
+	@Max(value = 5)
+	@Column(name = "DURATION_OF_STUDY")
 	public Integer getDurationOfStudy() {
 		return durationOfStudy;
 	}
@@ -140,23 +132,22 @@ public class StudyProgram implements Serializable {
 	public void setDurationOfStudy(Integer durationOfStudy) {
 		this.durationOfStudy = durationOfStudy;
 	}
-
+	
+	@Transient
 	public Integer getEcts() {
-		return ects;
+		return this.durationOfStudy * 60;
 	}
+	
+	@Transient
+	public Set<Professor> getProfessors() {
+		Set<Professor> professors = new HashSet<>();
 
-	public void setEcts(Integer ects) {
-		this.ects = ects;
-	}
-
-	public List<Professor> getProfessors(){
-		List<Professor> professors=new ArrayList<Professor>();
-		for(Subject subject: getSubjects()){
-			for(Professor professor: subject.getProfessors()){
-				if(!professors.contains(professor) && professor.getDateOfTermination()==null)
-					professors.add(professor);
+		for (Subject subject : this.subjects) {
+			for (Professor professor : subject.getProfessors()) {
+				professors.add(professor);
 			}
 		}
+
 		return professors;
 	}
 
@@ -164,10 +155,8 @@ public class StudyProgram implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((dateOfCreation == null) ? 0 : dateOfCreation.hashCode());
 		result = prime * result + ((department == null) ? 0 : department.hashCode());
 		result = prime * result + ((durationOfStudy == null) ? 0 : durationOfStudy.hashCode());
-		result = prime * result + ((ects == null) ? 0 : ects.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		return result;
@@ -182,11 +171,6 @@ public class StudyProgram implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		StudyProgram other = (StudyProgram) obj;
-		if (dateOfCreation == null) {
-			if (other.dateOfCreation != null)
-				return false;
-		} else if (!dateOfCreation.equals(other.dateOfCreation))
-			return false;
 		if (department == null) {
 			if (other.department != null)
 				return false;
@@ -196,11 +180,6 @@ public class StudyProgram implements Serializable {
 			if (other.durationOfStudy != null)
 				return false;
 		} else if (!durationOfStudy.equals(other.durationOfStudy))
-			return false;
-		if (ects == null) {
-			if (other.ects != null)
-				return false;
-		} else if (!ects.equals(other.ects))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -214,6 +193,5 @@ public class StudyProgram implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
