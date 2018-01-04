db435a79648a07b675c63995b98ecb98edb39ed4
Vesna S <vesnasdoboj@gmail.com>
2018-01-04 03:11:49 +0100

    Update Professor.java

--- College/src/main/java/college/model/Professor.java
+++ College/src/main/java/college/model/Professor.java
@@ -1,67 +1,54 @@
 package college.model;
 
-import java.util.*;
-import javax.persistence.CascadeType;
+import java.util.Set;
+import java.util.Date;
+import java.util.HashSet;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Transient;
 import javax.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 import org.springframework.format.annotation.DateTimeFormat;
+import college.enums.Gender;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "PROFESSORS")
 public class Professor extends Person {
 
-	private static final long serialVersionUID = 1L;
+	private String titleOfProfessor = "";
+	private Date dateOfEmployment = new Date();
+	private Set<Lecture> lectures = new HashSet<>();
+	private Set<Exam> exams = new HashSet<>();
+
+	public Professor() {
+	}
+
+	public Professor(String id, String fullName, String fatherName, Date dateOfBirth, String email, 
+			String telephone, Gender gender, Address address, String titleOfProfessor) {
+		super(id, fullName, fatherName, dateOfBirth, email, telephone, gender, address);
+		this.titleOfProfessor = titleOfProfessor;
+	}
 
 	@Pattern(regexp = "^([a-zA-Z]+\\s?){5,}$")
 	@Column(name = "TITLE")
-	private String title;
+	public String getTitleOfProfessor() {
+		return titleOfProfessor;
+	}
 
-	@NotNull
+	public void setTitleOfProfessor(String titleOfProfessor) {
+		this.titleOfProfessor = titleOfProfessor;
+	}
+
 	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	@Temporal(TemporalType.DATE)
 	@Column(name = "DATE_OF_EMPLOYMENT")
-	@Temporal(TemporalType.DATE)
-	private Date dateOfEmployment;
-
-	@DateTimeFormat(pattern = "dd-MM-yyyy")
-	@Column(name = "DATE_OF_TERMINATION", nullable = true)
-	@Temporal(TemporalType.DATE)
-	private Date dateOfTermination;
-
-	@OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<SubjectProfessor> subjectsProfessors;
-
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "professor")
-	private List<Exam> exams;
-
-	public Professor() {
-		exams = new ArrayList<Exam>();
-		subjectsProfessors = new ArrayList<SubjectProfessor>();
-	}
-
-	public Professor(String id, String firstName, String lastName, String fatherName, Date dateOfBirth, String email,
-			String telephone,String gender, Address address,String title, Date dateOfEmployment) {
-		super(id,firstName, lastName, fatherName, dateOfBirth, email, telephone, gender, address);
-		exams = new ArrayList<Exam>();
-		subjectsProfessors = new ArrayList<SubjectProfessor>();
-		this.title = title;
-		this.dateOfEmployment = dateOfEmployment;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 	public Date getDateOfEmployment() {
 		return dateOfEmployment;
 	}
@@ -70,78 +57,45 @@ public class Professor extends Person {
 		this.dateOfEmployment = dateOfEmployment;
 	}
 
-	public Date getDateOfTermination() {
-		return dateOfTermination;
+	@OneToMany(mappedBy = "professor", fetch = FetchType.LAZY)
+	@Cascade(CascadeType.ALL)
+	public Set<Lecture> getLectures() {
+		return lectures;
 	}
 
-	public void setDateOfTermination(Date dateOfTermination) {
-		this.dateOfTermination = dateOfTermination;
+	public void setLectures(Set<Lecture> lectures) {
+		this.lectures = lectures;
+	}
+	
+	public void addLecture(Lecture lecture) {
+		lecture.setProfessor(this);
+		this.lectures.add(lecture);
 	}
 
-	@NotEmpty
-	public List<SubjectProfessor> getSubjectsProfessors() {
-		return subjectsProfessors;
-	}
-
-	public List<Exam> getExams() {
+	@OneToMany(mappedBy = "professor", fetch = FetchType.LAZY)
+	@Cascade(CascadeType.ALL)
+	public Set<Exam> getExams() {
 		return exams;
 	}
 
-	public void setExams(List<Exam> exams) {
+	void setExams(Set<Exam> exams) {
 		this.exams = exams;
 	}
 
-	//set up list subjects for specified professor
-	public void setSubjects(List<Subject> subjects) {
-		this.subjectsProfessors.clear();
-		for(Subject subject:subjects)
-			this.subjectsProfessors.add(new SubjectProfessor(this, subject));
+	public void addExam(Exam exam) {
+		exam.setProfessor(this);
+		exams.add(exam);
 	}
 
-	//returns list subjects for specified professor
-	public List<Subject> getSubjects() {
-		List<Subject> subjects = new ArrayList<Subject>();
-		for (SubjectProfessor subProf : this.subjectsProfessors)
-			if(!subjects.contains(subProf.getSubject()))
-				subjects.add(subProf.getSubject());
+	@Transient
+	public Set<Subject> getSubjects() {
+		Set<Subject> subjects = new HashSet<>();
+
+		for (Lecture subjectProfessor : this.lectures) {
+			subjects.add(subjectProfessor.getSubject());
+		}
+
 		return subjects;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((dateOfEmployment == null) ? 0 : dateOfEmployment.hashCode());
-		result = prime * result + ((dateOfTermination == null) ? 0 : dateOfTermination.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Professor other = (Professor) obj;
-		if (dateOfEmployment == null) {
-			if (other.dateOfEmployment != null)
-				return false;
-		} else if (!dateOfEmployment.equals(other.dateOfEmployment))
-			return false;
-		if (dateOfTermination == null) {
-			if (other.dateOfTermination != null)
-				return false;
-		} else if (!dateOfTermination.equals(other.dateOfTermination))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
-			return false;
-		return true;
 	}
 
 }
