12ced436b2c9d4f3e3f83c5637c8e8a5f533d42e
Vesna S <vesnasdoboj@gmail.com>
2018-01-08 04:20:20 +0100

    Update ProfessorDaoImpl.java

--- College/src/main/java/college/dao/impl/extensions/ProfessorDaoImpl.java
+++ College/src/main/java/college/dao/impl/extensions/ProfessorDaoImpl.java
@@ -1,13 +1,12 @@
 package college.dao.impl.extensions;
 
-import org.hibernate.Query;
 import org.springframework.stereotype.Component;
 import college.dao.extensions.ProfessorDao;
 import college.dao.impl.HibernateDaoImpl;
 import college.model.Professor;
 
 @Component("professorDao")
-public class ProfessorDaoImpl extends HibernateDaoImpl<String, Professor> implements ProfessorDao {
+public class ProfessorDaoImpl extends HibernateDaoImpl<Professor> implements ProfessorDao {
 
 	public ProfessorDaoImpl() {
 		setEntityClass(Professor.class);
