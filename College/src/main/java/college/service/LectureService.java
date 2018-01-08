8ac4c5bfb3dfabbefdb85774d924ed4e1b6611e9
Vesna S <vesnasdoboj@gmail.com>
2018-01-08 04:24:01 +0100

    Create LectureService.java

---
+++ College/src/main/java/college/service/LectureService.java
@@ -0,0 +1,12 @@
+package college.service;
+
+import college.model.Lecture;
+
+public interface LectureService {
+		
+	Lecture findLectureById(String id);
+	
+	void saveOrUpdateLecture(Lecture lecture);
+	
+	void deleteLectureById(String id);
+}
