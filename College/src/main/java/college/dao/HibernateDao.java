06890bf443fe373a6baa7268b69b19ecf959f103
Vesna S <vesnasdoboj@gmail.com>
2018-01-08 04:12:33 +0100

    Update HibernateDao.java

--- College/src/main/java/college/dao/HibernateDao.java
+++ College/src/main/java/college/dao/HibernateDao.java
@@ -3,14 +3,14 @@ package college.dao;
 import java.io.Serializable;
 import java.util.Set;
 
-public interface HibernateDao<I extends Serializable, E extends Serializable> {
+public interface HibernateDao<E extends Serializable> {
 
 	Set<E> findAll();
 
-	E findOneById(final I i);
+	E findOneById(final String id);
 
 	void saveOrUpdate(E e);
 
-	void deleteById(final I i);
+	void deleteById(final String id);
 
 }
