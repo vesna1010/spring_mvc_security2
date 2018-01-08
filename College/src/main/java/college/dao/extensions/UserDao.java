23ce1586d1f266417b7fdfdcee98d251f8b1457a
Vesna S <vesnasdoboj@gmail.com>
2018-01-08 04:18:09 +0100

    Update UserDao.java

--- College/src/main/java/college/dao/extensions/UserDao.java
+++ College/src/main/java/college/dao/extensions/UserDao.java
@@ -3,7 +3,7 @@ package college.dao.extensions;
 import college.dao.HibernateDao;
 import college.model.User;
 
-public interface UserDao extends HibernateDao<String, User> {
+public interface UserDao extends HibernateDao<User> {
 
 	void disableByUsername(String username);
 }
