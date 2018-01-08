1bddc0555402de76afecfa14f6a035a528d4d5ce
Vesna S <vesnasdoboj@gmail.com>
2018-01-08 04:21:39 +0100

    Update SubjectDaoImpl.java

--- College/src/main/java/college/dao/impl/extensions/SubjectDaoImpl.java
+++ College/src/main/java/college/dao/impl/extensions/SubjectDaoImpl.java
@@ -6,7 +6,7 @@ import college.dao.impl.HibernateDaoImpl;
 import college.model.Subject;
 
 @Component("subjectDao")
-public class SubjectDaoImpl extends HibernateDaoImpl<String, Subject> implements SubjectDao {
+public class SubjectDaoImpl extends HibernateDaoImpl<Subject> implements SubjectDao {
 
 	public SubjectDaoImpl() {
 		setEntityClass(Subject.class);
