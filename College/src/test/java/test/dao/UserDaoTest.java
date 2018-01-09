7b9c9442cc742a4ab8501f6d4c5d3f9608a3fd7a
Vesna S <vesnasdoboj@gmail.com>
2018-01-09 02:06:05 +0100

    Create UserDaoTest.java

---
+++ College/src/test/java/test/dao/UserDaoTest.java
@@ -0,0 +1,94 @@
+package test.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import college.dao.extensions.UserDao;
+import college.enums.Role;
+import college.model.User;
+
+public class UserDaoTest extends BaseDaoTest {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Before
+	public void setUp() {
+		deleteAll();
+		userDao.saveOrUpdate(user1);
+		userDao.saveOrUpdate(user2);
+	}
+	
+	private void deleteAll() {
+		Set<User> users = userDao.findAll();
+		
+		for(User user: users) {
+			userDao.deleteById(user.getUsername());
+		}
+	}
+
+	@Test
+	public void findAllUsersTest() {
+		Set<User> users = userDao.findAll();
+
+		assertThat(users, hasSize(2));
+		assertTrue(users.contains(user1));
+		assertTrue(users.contains(user2));
+		assertFalse(users.contains(user3));
+	}
+	
+	@Test
+	public void findOneUserByUsernameTest() {
+		User user = userDao.findOneById("Username 1");
+
+		assertThat(user.getRoles(), hasSize(2));
+		assertThat(user.getEmail(), is("username1@gmail.com"));
+		assertTrue(user.isEnabled());
+	}
+	
+	@Test
+	public void saveUserTest() {
+		userDao.saveOrUpdate(user3);
+		
+		assertNotNull(userDao.findOneById("Username 3"));
+	}
+	
+
+	@Test
+	public void updateUserTest() {
+		user2.setRoles(new HashSet<>(Arrays.asList(Role.PROFESSOR)));
+		userDao.saveOrUpdate(user2);
+
+		User user = userDao.findOneById("Username 2");
+
+		assertThat(user.getRoles(), hasSize(1));
+	}
+	
+	@Test
+	public void deleteUserByUsernameTest() {
+		userDao.deleteById("Username 2");
+
+		assertNull(userDao.findOneById("Username 2"));
+	}
+
+	@Test
+	public void disableUserByUsernameTest() {
+		userDao.disableByUsername("Username 2");
+
+		User user = userDao.findOneById("Username 2");
+
+		assertFalse(user.isEnabled());
+	}
+
+}
+
