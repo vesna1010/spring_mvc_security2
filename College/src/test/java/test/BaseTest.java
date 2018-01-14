621718aa261e697227230e45968629b4a4e27c0c
Vesna S <vesnasdoboj@gmail.com>
2018-01-14 03:17:06 +0100

    Update BaseTest.java

--- College/src/test/java/test/BaseTest.java
+++ College/src/test/java/test/BaseTest.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
+
 import org.junit.Before;
+
 import college.enums.Gender;
 import college.enums.Role;
 import college.model.Address;
@@ -29,36 +32,47 @@ public abstract class BaseTest {
 	protected final User user3 = new User("Username 3", "username3@gmail.com", "Password3",
 			new HashSet<>(Arrays.asList(Role.USER, Role.PROFESSOR)));
 
-	protected final Department department1 = new Department("D1", "Department 1");
-	protected final Department department2 = new Department("D2", "Department 2");
+	protected final Department department1 = new Department("D1", "Department 1", 
+			new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime());
+	protected final Department department2 = new Department("D2", "Department 2",
+			new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime());
 
-	protected final StudyProgram studyProgram1 = new StudyProgram("SP1", "Study Program 1", department1);
-	protected final StudyProgram studyProgram2 = new StudyProgram("SP2", "Study Program 2", department1);
+	protected final StudyProgram studyProgram1 = new StudyProgram("SP1", "Study Program 1", 
+			new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime(), 3, department1);
+	protected final StudyProgram studyProgram2 = new StudyProgram("SP2", "Study Program 2", 
+			new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime(), 3, department1);
 
 	protected final Subject subject1 = new Subject("SUB1", "Subject 1", studyProgram1);
 	protected final Subject subject2 = new Subject("SUB2", "Subject 2", studyProgram1);
 
 	protected final Student student1 = new Student("S1", "First Student", "First Father Name",
 			new GregorianCalendar(1995, 5, 5).getTime(), "email1@gmail.com", "065-123-456", Gender.MALE,
-			new Address("City", "Street", "State"), studyProgram1);
+			new Address("City", "Street", "State"), new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime(),
+			1, studyProgram1);
 	protected final Student student2 = new Student("S2", "Second Student", "Second Father Name",
 			new GregorianCalendar(1995, 4, 4).getTime(), "email2@gmail.com", "065-124-156", Gender.MALE,
-			new Address("City", "Street", "State"), studyProgram1);
+			new Address("City", "Street", "State"), new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime(),
+			1, studyProgram1);
 
 	protected final Professor professor1 = new Professor("P1", "First Professor", "First Father Name",
 			new GregorianCalendar(1985, 3, 2).getTime(), "email3@gmail.com", "065-333-212", Gender.MALE,
-			new Address("City", "Street", "State"), "Title");
+			new Address("City", "Street", "State"), new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime(),
+			"Title");
 	protected final Professor professor2 = new Professor("P2", "Second Professor", "Second Father Name",
 			new GregorianCalendar(1985, 1, 5).getTime(), "email4@gmail.com", "065-423-515", Gender.MALE,
-			new Address("City", "Street", "State"), "Title");
+			new Address("City", "Street", "State"), new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime(),
+			"Title");
 
 	protected final Lecture lecture1 = new Lecture("L1", professor1, subject1, 2);
 	protected final Lecture lecture2 = new Lecture("L2", professor2, subject1, 2);
 	protected final Lecture lecture3 = new Lecture("L3", professor1, subject2, 2);
 
-	protected final Exam exam1 = new Exam("E1", student1, professor1, subject1, 8);
-	protected final Exam exam2 = new Exam("E2", student1, professor2, subject2, 9);
-	protected final Exam exam3 = new Exam("E3", student2, professor1, subject2, 9);
+	protected final Exam exam1 = new Exam("E1", student1, professor1, subject1,  
+			new GregorianCalendar(2017, Calendar.MAY, 1).getTime(), 8);
+	protected final Exam exam2 = new Exam("E2", student1, professor2, subject2,
+			new GregorianCalendar(2017, Calendar.MAY, 2).getTime(), 9);
+	protected final Exam exam3 = new Exam("E3", student2, professor1, subject2, 
+			new GregorianCalendar(2017, Calendar.MAY, 3).getTime(), 9);
 
 	public byte[] getImage() throws IOException, URISyntaxException {
 		File imageFile = new File(this.getClass().getResource("/images/image.jpg").toURI());
