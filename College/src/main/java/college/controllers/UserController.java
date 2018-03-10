7f9961d088ffda6ca0fa6cdac1d77b825071f1f8
Vesna S <vesnasdoboj@gmail.com>
2018-03-10 22:35:40 +0100

    Update UserController.java

--- College/src/main/java/college/controllers/UserController.java
+++ College/src/main/java/college/controllers/UserController.java
@@ -1,8 +1,6 @@
 package college.controllers;
 
 import java.security.Principal;
-import java.util.Arrays;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -13,22 +11,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import college.enums.Role;
 import college.model.User;
 import college.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
-	
-	
+
 	@Autowired
 	@Qualifier("userValidator")
 	private Validator validator;
 	@Autowired
 	private UserService userService;
-	private List<Role> roles = Arrays.asList(Role.values());
-	
+
 	@RequestMapping
 	public ModelAndView renderUsersPageWithAllUsers() {
 		return new ModelAndView("usersPage", "users", userService.findUsers());
@@ -36,22 +31,18 @@ public class UserController {
 
 	@RequestMapping("/userForm")
 	public ModelAndView renderEmptyUserForm(ModelAndView model) {
-		model.setViewName("userForm");
-		model.addObject("user", new User());
-		model.addObject("roles", roles);
-		
-		return model;
+		return new ModelAndView("userForm", "user", new User());
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public ModelAndView saveUserAndRenderUserForm(@ModelAttribute User user, BindingResult result) {
 		validator.validate(user, result);
-		
-		if(!result.hasErrors()) {
+
+		if (!result.hasErrors()) {
 			return saveUserAndGetModelAndView(user);
 		}
-		
-		return new ModelAndView("userForm", "roles", roles);
+
+		return new ModelAndView("userForm");
 	}
 
 	private ModelAndView saveUserAndGetModelAndView(User user) {
@@ -60,9 +51,9 @@ public class UserController {
 		return new ModelAndView("redirect:/users/userForm");
 	}
 
-	@RequestMapping("/delete/{username}")
-	public ModelAndView deleteUserAndRenderUsersPage(@PathVariable String username) {
-		userService.deleteUserByUsername(username);
+	@RequestMapping("/delete/{user}")
+	public ModelAndView deleteUserAndRenderUsersPage(@PathVariable User user) {
+		userService.deleteUser(user);
 
 		return new ModelAndView("redirect:/users");
 	}
@@ -76,20 +67,8 @@ public class UserController {
 
 	@RequestMapping("/changePassword")
 	public ModelAndView renderUserFormWithUser(Principal principal) {
-		ModelAndView model = new ModelAndView("userForm");
-
-		model.setViewName("userForm");
-		model.addObject("user", getUserWithoutPassword(principal.getName()));
-		model.addObject("roles", roles);
-
-		return model;
-	}
-	
-	private User getUserWithoutPassword(String username) {
-		User user = userService.findUserByUsername(username);
-		user.setPassword("");
-		
-		return user;
+		return new ModelAndView("userForm", "user", userService.findUserByUsernameWithoutPassword(principal.getName()));
 	}
 
 }
+
