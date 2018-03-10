41a9be1aa328ac45676bbd6360564c1106bfc223
Vesna S <vesnasdoboj@gmail.com>
2018-03-10 23:45:36 +0100

    Update ExamDaoImpl.java

--- College/src/main/java/college/dao/impl/extensions/ExamDaoImpl.java
+++ College/src/main/java/college/dao/impl/extensions/ExamDaoImpl.java
@@ -9,10 +9,11 @@ import college.dao.extensions.ExamDao;
 import college.dao.impl.HibernateDaoImpl;
 import college.model.Exam;
 import college.model.Professor;
+import college.model.StudyProgram;
 import college.model.Subject;
 
 @Component("examDao")
-public class ExamDaoImpl extends HibernateDaoImpl<Exam> implements ExamDao {
+public class ExamDaoImpl extends HibernateDaoImpl<String, Exam> implements ExamDao {
 
 	public ExamDaoImpl() {
 		setEntityClass(Exam.class);
@@ -21,14 +22,24 @@ public class ExamDaoImpl extends HibernateDaoImpl<Exam> implements ExamDao {
 	@SuppressWarnings("unchecked")
 	@Override
 	public Set<Exam> findExamsByObjects(Professor professor, Subject subject, Date date) {
-		Query query = getSession().createQuery(
-				"from Exam where professor=:professor and date=:date and subject=:subject");
-		
+		Query query = this.getSession()
+				.createQuery("from Exam where professor=:professor and date=:date and subject=:subject");
+
 		query.setEntity("professor", professor);
 		query.setEntity("subject", subject);
 		query.setDate("date", date);
-		
+
 		return new HashSet<Exam>(query.list());
 	}
-	
+
+	@Override
+	public void saveOrUpdate(Exam exam) {
+		StudyProgram studyProgram = exam.getStudyProgram();
+
+		exam.getStudent().setStudyProgram(studyProgram);
+		exam.getSubject().setStudyProgram(studyProgram);
+		this.getSession().saveOrUpdate(exam);
+	}
+
 }
+
