e9bff52de52d500073a95de9aa4c84db2954c8a0
Vesna S <vesnasdoboj@gmail.com>
2018-03-10 23:53:01 +0100

    Update Student.java

--- College/src/main/java/college/model/Student.java
+++ College/src/main/java/college/model/Student.java
@@ -26,24 +26,24 @@ import college.enums.Gender;
 @Table(name = "STUDENTS")
 public class Student extends Person {
 
-	private Date dateOfEntry = new Date();
-	private Integer yearOfStudy = 1;
-	private Double average = 5.0;
+	private Date dateOfEntry;
+	private Integer yearOfStudy;
+	private Double average;
 	private StudyProgram studyProgram;
 	private Set<Exam> exams = new HashSet<>();
 
 	public Student() {
 	}
 
-	public Student(String id, String fullName, String fatherName, Date dateOfBirth, String email, 
-			String telephone, Gender gender, Address address, Date dateOfEntry, Integer yearOfStudy,
-			StudyProgram studyProgram) {
+	public Student(String id, String fullName, String fatherName, Date dateOfBirth, String email, String telephone,
+			Gender gender, Address address, Date dateOfEntry, Integer yearOfStudy, StudyProgram studyProgram) {
 		super(id, fullName, fatherName, dateOfBirth, email, telephone, gender, address);
 		this.yearOfStudy = yearOfStudy;
 		this.dateOfEntry = dateOfEntry;
 		this.studyProgram = studyProgram;
 	}
 
+	@NotNull
 	@DateTimeFormat(pattern = "dd-MM-yyyy")
 	@Temporal(TemporalType.DATE)
 	@Column(name = "DATE_OF_ENTRY")
@@ -66,11 +66,11 @@ public class Student extends Person {
 	public void setYearOfStudy(Integer yearOfStudy) {
 		this.yearOfStudy = yearOfStudy;
 	}
-	
+
 	@NotNull
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "STUDY_PROGRAM_ID")
-	@Cascade(CascadeType.MERGE)
+	@Cascade(value = CascadeType.SAVE_UPDATE)
 	public StudyProgram getStudyProgram() {
 		return studyProgram;
 	}
@@ -80,15 +80,15 @@ public class Student extends Person {
 	}
 
 	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
-	@Cascade(CascadeType.ALL)
+	@Cascade(value = CascadeType.ALL)
 	public Set<Exam> getExams() {
 		return exams;
 	}
-	
-	void setExams(Set<Exam> exams) {
+
+	public void setExams(Set<Exam> exams) {
 		this.exams = exams;
 	}
-	
+
 	public void addExam(Exam exam) {
 		exam.setStudent(this);
 		this.exams.add(exam);
@@ -104,30 +104,5 @@ public class Student extends Person {
 		this.average = average;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((studyProgram == null) ? 0 : studyProgram.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		if (studyProgram == null) {
-			if (other.studyProgram != null)
-				return false;
-		} else if (!studyProgram.equals(other.studyProgram))
-			return false;
-		return true;
-	}
-	
 }
 
