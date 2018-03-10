6075de4c710f1b286abba3944fe684503a1b22c3
Vesna S <vesnasdoboj@gmail.com>
2018-03-10 23:53:36 +0100

    Update StudyProgram.java

--- College/src/main/java/college/model/StudyProgram.java
+++ College/src/main/java/college/model/StudyProgram.java
@@ -24,17 +24,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 import college.validation.MyId;
 import college.validation.Title;
 
-
 @SuppressWarnings({ "serial" })
 @Entity
 @Table(name = "STUDY_PROGRAMS")
 public class StudyProgram implements Serializable {
 
-	
-	private String id = "";
-	private String title = "";
-	private Date dateOfCreation = new Date();
-	private Integer durationOfStudy = 3;
+	private String id;
+	private String title;
+	private Date dateOfCreation;
+	private Integer durationOfStudy;
 	private Department department;
 	private Set<Student> students = new HashSet<>();
 	private Set<Subject> subjects = new HashSet<>();
@@ -42,8 +40,7 @@ public class StudyProgram implements Serializable {
 	public StudyProgram() {
 	}
 
-	public StudyProgram(String id, String title, Date dateOfCreation, Integer durationOfStudy, 
-			Department department) {
+	public StudyProgram(String id, String title, Date dateOfCreation, Integer durationOfStudy, Department department) {
 		this.id = id;
 		this.title = title;
 		this.dateOfCreation = dateOfCreation;
@@ -72,6 +69,7 @@ public class StudyProgram implements Serializable {
 		this.title = title;
 	}
 
+	@NotNull
 	@DateTimeFormat(pattern = "dd-MM-yyyy")
 	@Temporal(TemporalType.DATE)
 	@Column(name = "DATE_OF_CREATION")
@@ -86,7 +84,7 @@ public class StudyProgram implements Serializable {
 	@NotNull
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "DEPARTMENT_ID")
-	@Cascade(CascadeType.MERGE)
+	@Cascade(value = CascadeType.SAVE_UPDATE)
 	public Department getDepartment() {
 		return department;
 	}
@@ -96,30 +94,30 @@ public class StudyProgram implements Serializable {
 	}
 
 	@OneToMany(mappedBy = "studyProgram", fetch = FetchType.LAZY)
-	@Cascade(CascadeType.ALL)
+	@Cascade(value = CascadeType.ALL)
 	public Set<Student> getStudents() {
 		return students;
 	}
-	
-	void setStudents(Set<Student> students) {
+
+	public void setStudents(Set<Student> students) {
 		this.students = students;
 	}
-	
+
 	public void addStudent(Student student) {
 		student.setStudyProgram(this);
 		this.students.add(student);
 	}
 
 	@OneToMany(mappedBy = "studyProgram", fetch = FetchType.LAZY)
-	@Cascade(CascadeType.ALL)
+	@Cascade(value = CascadeType.ALL)
 	public Set<Subject> getSubjects() {
 		return subjects;
 	}
-	
-	void setSubjects(Set<Subject> subjects) {
+
+	public void setSubjects(Set<Subject> subjects) {
 		this.subjects = subjects;
 	}
-	
+
 	public void addSubject(Subject subject) {
 		subject.setStudyProgram(this);
 		this.subjects.add(subject);
@@ -136,12 +134,12 @@ public class StudyProgram implements Serializable {
 	public void setDurationOfStudy(Integer durationOfStudy) {
 		this.durationOfStudy = durationOfStudy;
 	}
-	
+
 	@Transient
 	public Integer getEcts() {
 		return this.durationOfStudy * 60;
 	}
-	
+
 	@Transient
 	public Set<Professor> getProfessors() {
 		Set<Professor> professors = new HashSet<>();
@@ -152,11 +150,11 @@ public class StudyProgram implements Serializable {
 
 		return professors;
 	}
-	
+
 	@Transient
 	public Set<Lecture> getLectures() {
 		Set<Lecture> lectures = new HashSet<>();
-		
+
 		for (Subject subject : this.subjects) {
 			lectures.addAll(subject.getLectures());
 		}
@@ -168,10 +166,7 @@ public class StudyProgram implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((department == null) ? 0 : department.hashCode());
-		result = prime * result + ((durationOfStudy == null) ? 0 : durationOfStudy.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		return result;
 	}
 
@@ -184,25 +179,10 @@ public class StudyProgram implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		StudyProgram other = (StudyProgram) obj;
-		if (department == null) {
-			if (other.department != null)
-				return false;
-		} else if (!department.equals(other.department))
-			return false;
-		if (durationOfStudy == null) {
-			if (other.durationOfStudy != null)
-				return false;
-		} else if (!durationOfStudy.equals(other.durationOfStudy))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
 			return false;
 		return true;
 	}
