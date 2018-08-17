280fa06f2d817a4e64bed834c48e0da619a495cb
Vesna S <vesnasdoboj@gmail.com>
2018-08-17 07:16:58 +0200

    Update HomeControllerTest.java

--- College/src/test/java/test/controllers/HomeControllerTest.java
+++ College/src/test/java/test/controllers/HomeControllerTest.java
@@ -31,7 +31,7 @@ public class HomeControllerTest extends BaseControllerTest {
 	public void renderLoginPageByAuthenticatedUserTest() throws Exception {
 		mockMvc.perform(get("/login"))
 		       .andExpect(status().isForbidden())
-	         .andExpect(forwardedUrl("/denied")); 
+	               .andExpect(forwardedUrl("/denied")); 
 	} 
 	
 }
