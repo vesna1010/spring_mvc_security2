1a80a1f0c71aac07e85aa07bcf635c5251c0cbad
Vesna <vesnasdoboj@gmail.com>
2019-09-01 15:44:32 +0200

    Update ProfessorConverter.java

--- College/src/main/java/college/converters/ProfessorConverter.java
+++ College/src/main/java/college/converters/ProfessorConverter.java
@@ -2,19 +2,17 @@ package college.converters;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 import college.model.Professor;
 import college.service.ProfessorService;
 
-@Component
 public class ProfessorConverter implements Converter<String, Professor> {
 
 	@Autowired
-	private ProfessorService service;
+	private ProfessorService professorService;
 
 	@Override
 	public Professor convert(String id) {
-		return (id == null ? null : service.findProfessorById(id));
+		return ((id == null || id.isEmpty()) ? null : professorService.findProfessorById(Long.valueOf(id)));
 	}
 
 }
