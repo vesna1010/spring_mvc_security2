757abedf76ff3d661225fdef6a99851e4a08a5a8
Vesna <vesnasdoboj@gmail.com>
2019-09-01 15:45:39 +0200

    Update StudyProgramConverter.java

--- College/src/main/java/college/converters/StudyProgramConverter.java
+++ College/src/main/java/college/converters/StudyProgramConverter.java
@@ -2,19 +2,17 @@ package college.converters;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 import college.model.StudyProgram;
 import college.service.StudyProgramService;
 
-@Component
 public class StudyProgramConverter implements Converter<String, StudyProgram> {
 
 	@Autowired
-	private StudyProgramService service;
+	private StudyProgramService studyProgramService;
 
 	@Override
 	public StudyProgram convert(String id) {
-		return (id == null ? null : service.findStudyProgramById(id));
+		return ((id == null || id.isEmpty()) ? null : studyProgramService.findStudyProgramById(Long.valueOf(id)));
 	}
 
 }
