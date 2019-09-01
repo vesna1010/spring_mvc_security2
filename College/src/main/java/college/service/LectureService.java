fb267f32fe18d5e519ec08260b69770a57b22efa
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:12:34 +0200

    Update LectureService.java

--- College/src/main/java/college/service/LectureService.java
+++ College/src/main/java/college/service/LectureService.java
@@ -1,14 +1,17 @@
 package college.service;
 
+import java.util.List;
 import college.model.Lecture;
+import college.model.ProfessorSubjectId;
+import college.model.StudyProgram;
 
 public interface LectureService {
 
-	Lecture findLectureById(Long id);
+	List<Lecture> findAllLecturesByStudyProgram(StudyProgram studyProgram);
 
 	void saveOrUpdateLecture(Lecture lecture);
 
-	void deleteLecture(Lecture lecture);
-	
+	void deleteLectureById(ProfessorSubjectId id);
+
 }
 
