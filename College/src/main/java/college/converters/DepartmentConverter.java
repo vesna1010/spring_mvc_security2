ca0fff81bc6753bb34620a676774b89210ada968
Vesna <vesnasdoboj@gmail.com>
2019-09-01 15:43:38 +0200

    Update DepartmentConverter.java

--- College/src/main/java/college/converters/DepartmentConverter.java
+++ College/src/main/java/college/converters/DepartmentConverter.java
@@ -2,19 +2,18 @@ package college.converters;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 import college.model.Department;
 import college.service.DepartmentService;
 
-@Component
 public class DepartmentConverter implements Converter<String, Department> {
 
 	@Autowired
-	private DepartmentService service;
+	private DepartmentService departmentService;
 
 	@Override
 	public Department convert(String id) {
-		return (id == null ? null : service.findDepartmentById(id));
+		return ((id == null || id.isEmpty()) ? null : departmentService.findDepartmentById(Long.valueOf(id)));
 	}
 
 }
+
