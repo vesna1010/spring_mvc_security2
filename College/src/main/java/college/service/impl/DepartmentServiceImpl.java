8d0befca880c7df821085f86e004ec49bacd723c
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:17:50 +0200

    Update DepartmentServiceImpl.java

--- College/src/main/java/college/service/impl/DepartmentServiceImpl.java
+++ College/src/main/java/college/service/impl/DepartmentServiceImpl.java
@@ -1,25 +1,31 @@
 package college.service.impl;
 
-import java.util.Set;
-import javax.annotation.Resource;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import college.dao.extensions.DepartmentDao;
+import org.springframework.transaction.annotation.Transactional;
+import college.dao.DepartmentDao;
 import college.model.Department;
 import college.service.DepartmentService;
 
 @Service
+@Transactional
 public class DepartmentServiceImpl implements DepartmentService {
 
-	@Resource
 	private DepartmentDao departmentDao;
 
+	@Autowired
+	public DepartmentServiceImpl(DepartmentDao departmentDao) {
+		this.departmentDao = departmentDao;
+	}
+
 	@Override
-	public Set<Department> findAllDepartments() {	
+	public List<Department> findAllDepartments() {
 		return departmentDao.findAll();
 	}
 
 	@Override
-	public Department findDepartmentById(String id) {
+	public Department findDepartmentById(Long id) {
 		return departmentDao.findById(id);
 	}
 
@@ -29,9 +35,8 @@ public class DepartmentServiceImpl implements DepartmentService {
 	}
 
 	@Override
-	public void deleteDepartment(Department department) {
-		departmentDao.delete(department);
+	public void deleteDepartmentById(Long id) {
+		departmentDao.deleteById(id);
 	}
 
 }
-
