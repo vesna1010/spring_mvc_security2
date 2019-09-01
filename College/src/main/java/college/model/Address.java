3c0c6dad661f178175ae205f32eaa0cddc0e2e14
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:03:13 +0200

    Update Address.java

--- College/src/main/java/college/model/Address.java
+++ College/src/main/java/college/model/Address.java
@@ -7,41 +7,23 @@ import javax.validation.constraints.Pattern;
 @Embeddable
 public class Address {
 
-	private String city;
 	private String street;
+	private String zipCode;
+	private String city;
 	private String state;
 
 	public Address() {
 	}
 
-	public Address(String city, String street, String state) {
-		this.city = city;
+	public Address(String street, String zipCode, String city, String state) {
 		this.street = street;
-		this.state = state;
-	}
-
-	@Pattern(regexp = "^([a-zA-Z]+\\s?){3,}$")
-	@Column(name = "STATE")
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	@Pattern(regexp = "^([a-zA-Z]+\\s?){3,}$")
-	@Column(name = "CITY")
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
+		this.zipCode = zipCode;
 		this.city = city;
+		this.state = state;
 	}
 
-	@Pattern(regexp = "^([a-zA-Z0-9]+\\s?){3,}$")
-	@Column(name = "STREET")
+	@Pattern(regexp = "^[a-zA-Z0-9\\.\\s]{5,}$")
+	@Column(name = "ADDRESS_STREET")
 	public String getStreet() {
 		return street;
 	}
@@ -50,10 +32,48 @@ public class Address {
 		this.street = street;
 	}
 
-	@Override
-	public String toString() {
-		return city + " - " + street + " - " + state;
+	@Pattern(regexp = "^\\d{5}$")
+	@Column(name = "ADDRESS_ZIP_CODE")
+	public String getZipCode() {
+		return zipCode;
 	}
 
-}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
 
+	@Pattern(regexp = "^[a-zA-Z\\s]{3,}$")
+	@Column(name = "ADDRESS_CITY")
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Pattern(regexp = "^[a-zA-Z\\s]{3,}$")
+	@Column(name = "ADDRESS_STATE")
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(street);
+		sb.append("\n");
+		sb.append(zipCode);
+		sb.append(" " + city);
+		sb.append("\n");
+		sb.append(state);
+
+		return sb.toString();
+	}
+	
+}
