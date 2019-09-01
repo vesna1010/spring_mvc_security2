7fcc108d508d3e3db6b38fd60ae9768dc8b41a08
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:06:39 +0200

    Update Professor.java

--- College/src/main/java/college/model/Professor.java
+++ College/src/main/java/college/model/Professor.java
@@ -1,8 +1,10 @@
 package college.model;
 
 import java.util.Set;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,19 +12,16 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 import org.springframework.format.annotation.DateTimeFormat;
 import college.enums.Gender;
+import college.validation.Title;
 
-@SuppressWarnings("serial")
 @Entity
 @Table(name = "PROFESSORS")
-public class Professor extends Person {
+public class Professor extends Person implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private Date dateOfEmployment;
 	private String titleOfProfessor;
 	private Set<Lecture> lectures = new HashSet<>();
@@ -31,14 +30,18 @@ public class Professor extends Person {
 	public Professor() {
 	}
 
-	public Professor(String id, String fullName, String fatherName, Date dateOfBirth, String email, String telephone,
+	public Professor(Long id, String fullName) {
+		super(id, fullName);
+	}
+
+	public Professor(Long id, String fullName, String fatherName, Date dateOfBirth, String email, String telephone,
 			Gender gender, Address address, Date dateOfEmployment, String titleOfProfessor) {
 		super(id, fullName, fatherName, dateOfBirth, email, telephone, gender, address);
 		this.dateOfEmployment = dateOfEmployment;
 		this.titleOfProfessor = titleOfProfessor;
 	}
 
-	@Pattern(regexp = "^([a-zA-Z]+\\s?){5,}$")
+	@Title
 	@Column(name = "TITLE")
 	public String getTitleOfProfessor() {
 		return titleOfProfessor;
@@ -60,8 +63,7 @@ public class Professor extends Person {
 		this.dateOfEmployment = dateOfEmployment;
 	}
 
-	@OneToMany(mappedBy = "professor", fetch = FetchType.LAZY)
-	@Cascade(value = CascadeType.ALL)
+	@OneToMany(mappedBy = "professor", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	public Set<Lecture> getLectures() {
 		return lectures;
 	}
@@ -70,13 +72,7 @@ public class Professor extends Person {
 		this.lectures = lectures;
 	}
 
-	public void addLecture(Lecture lecture) {
-		lecture.setProfessor(this);
-		this.lectures.add(lecture);
-	}
-
-	@OneToMany(mappedBy = "professor", fetch = FetchType.LAZY)
-	@Cascade(value = CascadeType.ALL)
+	@OneToMany(mappedBy = "professor", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	public Set<Exam> getExams() {
 		return exams;
 	}
@@ -85,21 +81,4 @@ public class Professor extends Person {
 		this.exams = exams;
 	}
 
-	public void addExam(Exam exam) {
-		exam.setProfessor(this);
-		exams.add(exam);
-	}
-
-	@Transient
-	public Set<Subject> getSubjects() {
-		Set<Subject> subjects = new HashSet<>();
-
-		for (Lecture subjectProfessor : this.lectures) {
-			subjects.add(subjectProfessor.getSubject());
-		}
-
-		return subjects;
-	}
-
 }
-
