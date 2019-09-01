e3222a79e9974b27c4adf62225ed30a5800245c1
Vesna <vesnasdoboj@gmail.com>
2019-09-01 15:46:45 +0200

    Delete DepartmentDao.java

--- College/src/main/java/college/dao/extensions/DepartmentDao.java
+++
@@ -1,9 +0,0 @@
-package college.dao.extensions;
-
-import college.dao.HibernateDao;
-import college.model.Department;
-
-public interface DepartmentDao extends HibernateDao<String, Department> {
-
-}
-
