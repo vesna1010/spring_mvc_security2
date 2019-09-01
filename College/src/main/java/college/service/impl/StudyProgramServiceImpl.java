a149a57294c76444d36aec83b90f838a42527cd8
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:20:27 +0200

    Update StudyProgramServiceImpl.java

--- College/src/main/java/college/service/impl/StudyProgramServiceImpl.java
+++ College/src/main/java/college/service/impl/StudyProgramServiceImpl.java
@@ -1,31 +1,38 @@
 package college.service.impl;
 
-import java.util.Set;
-import javax.annotation.Resource;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import college.dao.extensions.StudyProgramDao;
+import org.springframework.transaction.annotation.Transactional;
+import college.dao.StudyProgramDao;
+import college.model.Department;
 import college.model.StudyProgram;
 import college.service.StudyProgramService;
 
 @Service
+@Transactional
 public class StudyProgramServiceImpl implements StudyProgramService {
 
-	@Resource
 	private StudyProgramDao studyProgramDao;
 
+	@Autowired
+	public StudyProgramServiceImpl(StudyProgramDao studyProgramDao) {
+		this.studyProgramDao = studyProgramDao;
+	}
+
 	@Override
-	public Set<StudyProgram> findAllStudyPrograms() {
+	public List<StudyProgram> findAllStudyPrograms() {
 		return studyProgramDao.findAll();
 	}
 
 	@Override
-	public StudyProgram findStudyProgramById(String id) {
-		return studyProgramDao.findById(id);
+	public List<StudyProgram> findAllStudyProgramsByDepartment(Department department) {
+		return studyProgramDao.findAllByDepartment(department);
 	}
 
 	@Override
-	public void deleteStudyProgram(StudyProgram studyProgram) {
-		studyProgramDao.delete(studyProgram);
+	public StudyProgram findStudyProgramById(Long id) {
+		return studyProgramDao.findById(id);
 	}
 
 	@Override
@@ -33,5 +40,9 @@ public class StudyProgramServiceImpl implements StudyProgramService {
 		studyProgramDao.saveOrUpdate(studyProgram);
 	}
 
-}
+	@Override
+	public void deleteStudyProgramById(Long id) {
+		studyProgramDao.deleteById(id);
+	}
 
+}
