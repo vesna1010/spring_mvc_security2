f52a25084de50cfe736a974d14e900c41a4521ad
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:20:53 +0200

    Update SubjectServiceImpl.java

--- College/src/main/java/college/service/impl/SubjectServiceImpl.java
+++ College/src/main/java/college/service/impl/SubjectServiceImpl.java
@@ -1,31 +1,38 @@
 package college.service.impl;
 
-import java.util.Set;
-import javax.annotation.Resource;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import college.dao.extensions.SubjectDao;
+import org.springframework.transaction.annotation.Transactional;
+import college.dao.SubjectDao;
+import college.model.StudyProgram;
 import college.model.Subject;
 import college.service.SubjectService;
 
 @Service
+@Transactional
 public class SubjectServiceImpl implements SubjectService {
 
-	@Resource
 	private SubjectDao subjectDao;
 
+	@Autowired
+	public SubjectServiceImpl(SubjectDao subjectDao) {
+		this.subjectDao = subjectDao;
+	}
+
 	@Override
-	public Set<Subject> findAllSubjects() {
+	public List<Subject> findAllSubjects() {
 		return subjectDao.findAll();
 	}
 
 	@Override
-	public Subject findSubjectById(String id) {
-		return subjectDao.findById(id);
+	public List<Subject> findAllSubjectsByStudyProgram(StudyProgram studyProgram) {
+		return subjectDao.findAllByStudyProgram(studyProgram);
 	}
 
 	@Override
-	public void deleteSubject(Subject subject) {
-		subjectDao.delete(subject);
+	public Subject findSubjectById(Long id) {
+		return subjectDao.findById(id);
 	}
 
 	@Override
@@ -33,5 +40,9 @@ public class SubjectServiceImpl implements SubjectService {
 		subjectDao.saveOrUpdate(subject);
 	}
 
-}
+	@Override
+	public void deleteSubjectById(Long id) {
+		subjectDao.deleteById(id);
+	}
 
+}
