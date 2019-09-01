fd46413ff65ccf3b343b9e1dd08f6c65da178c37
Vesna <vesnasdoboj@gmail.com>
2019-09-01 17:04:48 +0200

    Update StudyProgramDaoTest.java

--- College/src/test/java/test/dao/StudyProgramDaoTest.java
+++ College/src/test/java/test/dao/StudyProgramDaoTest.java
@@ -1,91 +1,66 @@
 package test.dao;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import java.util.Set;
-import org.junit.Before;
+import java.util.List;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import college.dao.extensions.StudyProgramDao;
+import college.dao.StudyProgramDao;
+import college.model.Department;
 import college.model.StudyProgram;
 
-public class StudyProgramDaoTest extends BaseDaoTest{
-	
+public class StudyProgramDaoTest extends BaseDaoTest {
+
 	@Autowired
 	private StudyProgramDao studyProgramDao;
 
-	@Before
-	public void setUp() {
-		studyProgramDao.deleteAll();
-		subject1.addLecture(lecture1);
-		subject1.addLecture(lecture2);
-		studyProgram1.addStudent(student1);
-		studyProgram1.addStudent(student2);
-		studyProgram1.addSubject(subject1);
-		studyProgram1.addSubject(subject2);
-		studyProgramDao.saveOrUpdate(studyProgram1);
-	}
-	
 	@Test
 	public void findAllStudyProgramsTest() {
-		Set<StudyProgram> studyPrograms = studyProgramDao.findAll();
+		List<StudyProgram> studyPrograms = studyProgramDao.findAll();
 
-		assertThat(studyPrograms, hasSize(1));
-		assertTrue(studyPrograms.contains(studyProgram1));
-		assertFalse(studyPrograms.contains(studyProgram2));
+		assertThat(studyPrograms, hasSize(3));
 	}
-	
+
+	@Test
+	public void findAllByDepartmentTest() {
+		Department department = new Department(1L, "Department A");
+		
+		List<StudyProgram> studyPrograms = studyProgramDao.findAllByDepartment(department);
+
+		assertThat(studyPrograms, hasSize(2));
+	}
+
 	@Test
 	public void findStudyProgramByIdTest() {
-		StudyProgram studyProgram = studyProgramDao.findById("SP1");
+		StudyProgram studyProgram = studyProgramDao.findById(1L);
 
-		assertThat(studyProgram.getTitle(), is("Study Program 1"));
-		assertThat(studyProgram.getDepartment(), equalTo(department1));
-		assertThat(studyProgram.getStudents(), hasSize(2));
-		assertThat(studyProgram.getSubjects(), hasSize(2));
-		assertThat(studyProgram.getLectures(), hasSize(2));
-		assertThat(studyProgram.getProfessors(), hasSize(2));
-	}
-
-	@Test
-	public void saveStudyProgramTest() {
-		studyProgramDao.saveOrUpdate(studyProgram2);
-		
-		assertNotNull(studyProgramDao.findById("SP2"));
-	}
-	
-	@Test
-	public void updateStudyProgramTest() {
-		studyProgram1.setDepartment(department2);
-		
-		studyProgramDao.saveOrUpdate(studyProgram1);
-		
-		StudyProgram studyProgram = studyProgramDao.findById("SP1");
-
-		assertThat(studyProgram.getTitle(), is("Study Program 1"));
-		assertThat(studyProgram.getDepartment(), equalTo(department2));
+		assertThat(studyProgram.getTitle(), is("Study Program A"));
 		assertThat(studyProgram.getStudents(), hasSize(2));
 		assertThat(studyProgram.getSubjects(), hasSize(2));
 	}
-	
+
+	@Test
+	public void saveOrUpdateStudyProgramTest() {
+		StudyProgram studyProgram = studyProgramDao.findById(1L);
+
+		studyProgram.setTitle("Study Program");
+
+		studyProgramDao.saveOrUpdate(studyProgram);
+
+		studyProgram = studyProgramDao.findById(1L);
+
+		assertThat(studyProgram.getTitle(), is("Study Program"));
+		assertThat(studyProgram.getStudents(), hasSize(2));
+		assertThat(studyProgram.getSubjects(), hasSize(2));
+	}
+
 	@Test
 	public void deleteStudyProgramByIdTest() {
-		studyProgramDao.deleteById("SP1");
-		
-		assertNull(studyProgramDao.findById("SP1"));
-	}
-	
-	@Test
-	public void deleteStudyProgramTest() {
-		studyProgramDao.delete(studyProgram1);
-		
-		assertNull(studyProgramDao.findById("SP1"));
-	}
-}
+		studyProgramDao.deleteById(1L);
 
+		assertNull(studyProgramDao.findById(1L));
+	}
+
+}
