def30db8dc3b8abdad4a8da97ad25771c87d099c
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:30:39 +0200

    Update activeClass.js

--- College/src/main/webapp/resource/jQuery/activeClass.js
+++ College/src/main/webapp/resource/jQuery/activeClass.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	
+
 	var active = $("[name='page']").val();
-	
+
 	$("ul.navbar-nav li").each(function() {
 		if ($(this).attr("id") == active) {
 			$(this).addClass("active");
 		}
 	});
-	
+
 });
