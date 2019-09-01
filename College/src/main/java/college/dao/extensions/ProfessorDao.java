72957a265bcb3453d4df2c5484f7d6fdfa0f7d5a
Vesna <vesnasdoboj@gmail.com>
2019-09-01 15:47:13 +0200

    Delete ProfessorDao.java

--- College/src/main/java/college/dao/extensions/ProfessorDao.java
+++
@@ -1,9 +0,0 @@
-package college.dao.extensions;
-
-import college.dao.HibernateDao;
-import college.model.Professor;
-
-public interface ProfessorDao extends HibernateDao<String, Professor> {
-
-}
-
