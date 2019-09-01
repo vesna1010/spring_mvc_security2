e95a94e8918844a7bb59d0030348df3ee53c3d46
Vesna <vesnasdoboj@gmail.com>
2019-09-01 17:02:17 +0200

    Update BaseDaoTest.java

--- College/src/test/java/test/dao/BaseDaoTest.java
+++ College/src/test/java/test/dao/BaseDaoTest.java
@@ -1,13 +1,19 @@
 package test.dao;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import test.BaseTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
-public abstract class BaseDaoTest extends BaseTest {
+@Sql(scripts = "classpath:db/db.sql")
+@Transactional
+@Commit
+public abstract class BaseDaoTest {
 
 }
+
 
