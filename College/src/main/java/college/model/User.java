c6540ac1edf0bd0527c7d8671418f90150452155
Vesna <vesnasdoboj@gmail.com>
2019-09-01 16:10:40 +0200

    Update User.java

--- College/src/main/java/college/model/User.java
+++ College/src/main/java/college/model/User.java
@@ -2,6 +2,7 @@ package college.model;
 
 import java.io.Serializable;
 import java.util.Set;
+import java.util.Collection;
 import java.util.HashSet;
 import javax.persistence.CollectionTable;
 import javax.persistence.JoinColumn;
@@ -10,43 +11,83 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import college.enums.Role;
 
-@SuppressWarnings({ "serial" })
 @Entity
 @Table(name = "USERS")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
 
-	private String username;
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private String email;
 	private String password;
 	private String confirmPassword;
-	private String email;
-	private Boolean enabled = true;
+	private boolean enabled = true;
 	private Set<Role> roles = new HashSet<>();
 
 	public User() {
 	}
 
-	public User(String username, String email, String password, Set<Role> roles) {
-		this.username = username;
-		this.password = password;
+	public User(Long id, String email) {
+		this.id = id;
 		this.email = email;
+	}
+
+	public User(Long id, String name, String email, String password, String confirmPassword, boolean enabled,
+			Set<Role> roles) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.confirmPassword = confirmPassword;
+		this.enabled = enabled;
 		this.roles = roles;
 	}
 
 	@Id
-	@Column(name = "USERNAME")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	@Transient
 	public String getUsername() {
-		return username;
+		return email;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	@Column(name = "EMAIL")
+	public String getEmail() {
+		return email;
 	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
 	@Column(name = "PASSWORD")
 	public String getPassword() {
 		return password;
@@ -65,26 +106,32 @@ public class User implements Serializable {
 		this.confirmPassword = confirmPassword;
 	}
 
-	@Column(name = "EMAIL")
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
+	@Override
 	@Column(name = "ENABLED")
-	public Boolean getEnabled() {
+	public boolean isEnabled() {
 		return enabled;
 	}
 
-	public void setEnabled(Boolean enabled) {
+	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
 
+	@Transient
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> authorities = new HashSet<>();
+
+		for (Role role : roles) {
+			authorities.add(role);
+		}
+
+		return authorities;
+	}
+
 	@ElementCollection
-	@CollectionTable(name = "USERS_ROLES", joinColumns = @JoinColumn(name = "USERNAME"))
+	@CollectionTable(
+			name = "USERS_ROLES", 
+			joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"))
 	@Enumerated(EnumType.STRING)
 	@Column(name = "ROLE")
 	public Set<Role> getRoles() {
@@ -95,11 +142,29 @@ public class User implements Serializable {
 		this.roles = roles;
 	}
 
+	@Transient
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Transient
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Transient
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		return result;
 	}
 
@@ -112,13 +177,12 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (username == null) {
-			if (other.username != null)
+		if (email == null) {
+			if (other.email != null)
 				return false;
-		} else if (!username.equals(other.username))
+		} else if (!email.equals(other.email))
 			return false;
 		return true;
 	}
 
 }
-
