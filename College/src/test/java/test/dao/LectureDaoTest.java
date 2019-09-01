68287bf9d4eac6a2a529d000706fdefdc7070e95
Vesna <vesnasdoboj@gmail.com>
2019-09-01 17:03:33 +0200

    Update LectureDaoTest.java

--- College/src/test/java/test/dao/LectureDaoTest.java
+++ College/src/test/java/test/dao/LectureDaoTest.java
@@ -2,80 +2,67 @@ package test.dao;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import java.util.Set;
-import org.junit.Before;
+import java.util.List;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import college.dao.extensions.LectureDao;
+import college.dao.LectureDao;
 import college.model.Lecture;
+import college.model.ProfessorSubjectId;
+import college.model.StudyProgram;
 
 public class LectureDaoTest extends BaseDaoTest {
 
 	@Autowired
 	private LectureDao lectureDao;
 
-	@Before
-	public void setUp() {
-		lectureDao.deleteAll();
-		lectureDao.saveOrUpdate(lecture1);
-		lectureDao.saveOrUpdate(lecture2);
+	@Test
+	public void findAllTest() {
+		List<Lecture> lectures = lectureDao.findAll();
+
+		assertThat(lectures, hasSize(4));
 	}
 
 	@Test
-	public void findAllLectures() {
-		Set<Lecture> lectures = lectureDao.findAll();
+	public void findAllByStudyProgramIdTest() {
+		StudyProgram studyProgram = new StudyProgram(1L, "Study Program A");
+		
+		List<Lecture> lectures = lectureDao.findAllByStudyProgram(studyProgram);
 
-		assertThat(lectures, hasSize(2));
-		assertTrue(lectures.contains(lecture1));
-		assertTrue(lectures.contains(lecture2));
-		assertFalse(lectures.contains(lecture3));
+		assertThat(lectures, hasSize(3));
 	}
 
 	@Test
 	public void findLectureByIdTest() {
-		Lecture lecture = lectureDao.findById(lecture1.getId());
+		ProfessorSubjectId lectureId = new ProfessorSubjectId(4L, 1L);
+		
+		Lecture lecture = lectureDao.findById(lectureId);
 
-		assertThat(lecture.getProfessor(), is(professor1));
-		assertThat(lecture.getSubject(), is(subject1));
+		assertThat(lecture.getHours(), is(2));
 	}
 
 	@Test
-	public void saveLectureTest() {
-		lectureDao.saveOrUpdate(lecture3);
+	public void saveOrUpdateLectureTest() {
+		ProfessorSubjectId lectureId = new ProfessorSubjectId(4L, 1L);
+		Lecture lecture = lectureDao.findById(lectureId);
 
-		assertNotNull(lecture3.getId());
-	}
+		lecture.setHours(4);
 
-	@Test
-	public void updateLectureTest() {
-		lecture1.setProfessor(professor2);
-		lecture1.setSubject(subject2);
+		lectureDao.saveOrUpdate(lecture);
 
-		lectureDao.saveOrUpdate(lecture1);
+		lecture = lectureDao.findById(lectureId);
 
-		Lecture lecture = lectureDao.findById(lecture1.getId());
-
-		assertThat(lecture.getProfessor(), is(professor2));
-		assertThat(lecture.getSubject(), is(subject2));
+		assertThat(lecture.getHours(), is(4));
 	}
 
 	@Test
 	public void deleteLectureByIdTest() {
-		lectureDao.deleteById(lecture1.getId());
+		ProfessorSubjectId lectureId = new ProfessorSubjectId(4L, 1L);
 
-		assertNull(lectureDao.findById(lecture1.getId()));
-	}
-	
-	@Test
-	public void deleteLectureTest() {
-		lectureDao.delete(lecture1);
+		lectureDao.deleteById(lectureId);
 
-		assertNull(lectureDao.findById(lecture1.getId()));
+		assertNull(lectureDao.findById(lectureId));
 	}
 
 }
